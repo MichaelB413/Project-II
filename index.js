445b18c91adec2dbb57f51f0406ad2b80de624c1
Michael Bonner <michaelbonner413@gmail.com>
2024-10-08 17:46:44 -0400

    Stirke through

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 
 function addtolist(){
     let formValue = document.querySelector("#Chores").value;
+
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox';
+    checkbox.setAttribute("onClick", "strikeOut()")
     const listTag = document.createElement("li");
+    listTag.appendChild(checkbox);
     const innerText = document.createTextNode(formValue);
     listTag.appendChild(innerText);
     const listElement = document.querySelector("ol");
-    listElement.appendChild(listTag)
+    listElement.insertBefore(listTag, listElement.firstChild);
+    var setter = document.querySelector("li")
 }
+
+function strikeOut(){
+    event.currentTarget.parentNode.style.textDecoration = "line-through";
+   console.log(event.target);
+
+}
+
+
+
+
+
+
+
